37bbc88b3ea4d7ea5c380b1de30753c3a121c4c2
sankarsv <sankarsv@192.168.1.11>
2020-04-02 13:38:39 +0530

    Spring Initializer changes

---
+++ src/main/java/com/app/aims/AimsApplication.java
@@ -0,0 +1,13 @@
+package com.app.aims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimsApplication.class, args);
+	}
+
+}
